5d82266e280472782e0fbe2c6f460c771bbb5773
lumilio <logistic-division@lumilium.com>
2021-11-03 21:34:17 +0100

    risolto problema del mostrare la rispettiva chat

--- milestone-2/asset/js/main.js
+++ milestone-2/asset/js/main.js
@@ -16,13 +16,16 @@ const app = new Vue({
     el: '#root',
 
     data: { 
+
+        active: 0,
+
         contacts: [
 
             {
                 name: 'Michele',
                 foto: './avatar-pics/avatar_1.jpg',
                 avatar: '_1',
-                visible: true,
+                visible: false,
                 messages: [
                     {
                         date: '10/01/2020 15:30:55',
@@ -46,7 +49,7 @@ const app = new Vue({
                 name: 'Fabio',
                 foto: './avatar-pics/avatar_2.jpg',
                 avatar: '_2',
-                visible: true,
+                visible: false,
                 messages: [
                     {
                         date: '20/03/2020 16:30:00',
@@ -70,7 +73,7 @@ const app = new Vue({
                 name: 'Samuele',
                 foto: './avatar-pics/avatar_3.jpg',
                 avatar: '_3',
-                visible: true,
+                visible: false,
                 messages: [
                     {
                         date: '28/03/2020 10:10:40',
@@ -94,7 +97,7 @@ const app = new Vue({
                 name: 'Luisa',
                 foto: './avatar-pics/avatar_6.jpg',
                 avatar: '_4',
-                visible: true,
+                visible: false,
                 messages: [
                     {
                         date: '10/01/2020 15:30:55',
@@ -112,18 +115,41 @@ const app = new Vue({
     },
 
 
-
-
-
-
-
-
     methods: { 
-        
+        selectChat(x){
+            app.active = x;
+        }  
     },
 
     
 })
+
+
+
+
+
+
+/* primo tentativo */
+
+/* for (let i = 0; i < app.contacts.length; i++) {
+    if(app.contacts[i] != app.contacts[x]){
+        app.contacts[i].visible = false;
+    } else{
+        app.contacts[i].visible = true;
+    } 
+    app.active = x;
+} */
+
+
+
+
+
+
+
+
+
+
+
 
 
 
