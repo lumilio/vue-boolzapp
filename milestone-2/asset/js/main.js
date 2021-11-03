2ef6afc862332881ae964a7a872b2bcfad3da2df
lumilio <logistic-division@lumilium.com>
2021-11-03 18:26:28 +0100

    aggiunte direttive

--- milestone-2/asset/js/main.js
+++ milestone-2/asset/js/main.js
@@ -1,11 +1,8 @@
 /* 
 
-    ● Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e
-     dall’interlocutore (bianco) assegnando due classi CSS diverse
-
-
-    ● Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare
-    nome e immagine di ogni contatto 
+  ● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
+    messaggi relativi al contatto attivo all’interno del pannello della conversazione
+  ● Click sul contatto mostra la conversazione del contatto cliccato  
     
 */
 
