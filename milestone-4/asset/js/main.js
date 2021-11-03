e91d5303aac2b433b1b671f2d0fc9d6090aa44ea
lumilio <logistic-division@lumilium.com>
2021-11-03 22:53:42 +0100

    aggiunta direttiva

--- milestone-4/asset/js/main.js
+++ milestone-4/asset/js/main.js
@@ -1,9 +1,8 @@
 /* 
 
-  ● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
-    “enter” il testo viene aggiunto al thread sopra, come messaggio verde
-  ● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
-    un “ok” come risposta, che apparirà dopo 1 secondo. 
+  ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i
+    contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo
+    “mar” rimangono solo Marco e Martina)
     
 */
 
